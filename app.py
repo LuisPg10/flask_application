9f97cb009af08107728b83cb1ccc8b8449ec1fcd
Luis Pinto <dapin46@hotmail.com>
2022-08-19 13:09:34 -0500

    Quitando el debug de app

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def session():
     elif request.method == "POST" and passed:
         return render_template("reservation.html", title = f"Bienvenido")
 
-app.run(host = "localhost", port = 5000, debug = True)
+app.run(host = "localhost", port = 5000)
