d3ab533e3cd498664c4285643897718a14d2396c
Luis Pinto <dapin46@hotmail.com>
2022-08-19 11:13:21 -0500

    Acciones sobre la base de datos

--- database.py
+++ database.py
@@ -9,13 +9,22 @@ def sql_connection():
         print(Error)
 
 def sql_add_user(email, name, last_name, password):
-    strsql = ("INSERT INTO USERS ("+
-    "email, name, last name, password) VALUES (?,?,?,?)", 
-    (email, name, last_name, password))
+    strsql = f"INSERT INTO Users (email, name, last_name, password) VALUES ('{email}', '{name}', '{last_name}', '{password}');"
 
     data = sql_connection()
     cursor_obj = data.cursor()
-    cursor_obj.execute(f"{strsql}")
+    cursor_obj.execute(strsql)
 
     data.commit()
     data.close()
+
+def sql_check_user():
+    strsql = "SELECT email, password FROM Users;"
+
+    data = sql_connection()
+    cursor_obj = data.cursor()
+    cursor_obj.execute(strsql)
+    chek_user = cursor_obj.fetchall()
+    data.close()
+
+    return chek_user
